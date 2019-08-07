c370dfc7f0548172acc8d9fda1706f0e482668ed
Damhan Richardson <damhanrichardson@gmail.com>
2019-08-07 12:46:59 +0100

    Version 1.0 for EUW only.

---
+++ README.md
@@ -0,0 +1,20 @@
+# trackerGGTop
+
+A Web scraper written using selenium in python.
+
+Takes the top 100 tft players from tracker.gg and writes them to a CSV.
+
+# Install & Run
+
+- Clone repo
+- Pip install selenium
+- Install the latest chromedriver from online.
+- CD into directory.
+- python3 main.py
+
+
+
+# Disclaimer
+I am not affiliated nor represent tracker.gg.
+
+This program has no commercial purpose and is for educational & personel purposes only.
---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import csv
+
+#Start up a new chrome driver
+driver = webdriver.Chrome()
+#Get our desired URL
+driver.get('https://tracker.gg/tft/leaderboards/stats/all/default?page=1&region=EUW')
+
+#grab the table
+table = driver.find_element_by_css_selector('#app > div.trn-wrapper > div.trn-container > div > main > div.leaderboards > div.content > div > div > div.board > table')
+
+#Open our CSV file.
+with open('results.csv', 'w', newline='') as csvFile:
+    wr = csv.writer(csvFile)
+    #Iterate throw each row, writing to our CSV.
+    for row in table.find_elements_by_css_selector('tr'):
+        wr.writerow([cell.text for cell in row.find_elements_by_css_selector('td')])
